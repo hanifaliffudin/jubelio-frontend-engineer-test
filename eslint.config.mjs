06db290dd81f8095de36b51765fdd740471ad813
Hanif Aliffudin <68997219+hanifaliffudin@users.noreply.github.com>
2024-08-31 14:02:28 +0700

    setup eslint prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  ...tseslint.configs.recommended,
+];
